2172d6f4eabdfcd335090abe5c9b5b00b6e6e7d4
Dwi Prayoga <>
2023-04-14 00:41:23 +0800

    database user admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,10 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('id')->primary;
+            $table->string('name', 255)->nullable(false);
+            $table->string('username', 255)->nullable(false);
+            $table->string('email')->nullable(false);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();

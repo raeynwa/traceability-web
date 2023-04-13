025b0964cdc89e7ba51283284cde6d83d2bea6ab
Dwi Prayoga <>
2023-04-14 01:01:01 +0800

    database produk dan detail produk

---
+++ database/migrations/2023_04_13_165438_create_produk_tabel.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->integer('id')->primery();
+            $table->integer('jenis_produk')->nullable(false);
+            $table->string('nama_produk', 255)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produk_tabel');
+    }
+};

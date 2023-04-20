28bd535f28edee026dd26986b1f974670bbd91a6
Dwi Prayoga <>
2023-04-20 18:18:45 +0800

    buat model database

---
+++ app/Models/DetailProduk.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetailProduk extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id',
+        'id_produk',
+        'kode_produk',
+        'jenis_produk',
+        'nama_produk',
+        'nama_petani',
+        'teknik_budidaya',
+        'lokasi_tanam',
+        'tanggal_tanam',
+        'usia_bulan',
+        'usia_hari',
+        'kualitas_produk',
+        'tanggal_expired'
+    ];
+}
